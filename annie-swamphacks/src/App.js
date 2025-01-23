80e3835d92ad4447142aff76d69b59925c3de0a6
AnanyaKakaveti <a.kakaveti@ufl.edu>
2025-01-23 15:34:11 -0500

    Basic personal website established

--- annie-swamphacks/src/App.js
+++ annie-swamphacks/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import pfp from './Assets/Annie_LinkedIn.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/* create a left and right element to create a more interesting page */}
+        <div className="left">
+          <img src={pfp} className="pfp-img"/>
+        </div>
+
+        <div className="right">
+          <h1>Annie Kakaveti</h1>
+          <div className = "tagline">A senior CS student at UF, looking for a full-time role</div>
+        </div>
       </header>
     </div>
   );
